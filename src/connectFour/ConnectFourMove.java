9716bda35646b8126a05853949c533d40eac70eb
Andreas Lindmark <kgs@Update.UU.SE>
2016-06-22 18:14:12 +0200

    Removed unneeded methods

--- src/connectFour/ConnectFourMove.java
+++ src/connectFour/ConnectFourMove.java
@@ -13,20 +13,9 @@ public class ConnectFourMove implements Move {
 		this.row = row;
 	}
 	
-	@Override
 	public String toString(){
 		StringBuilder strb = new StringBuilder();
 		strb.append(row);
 		return strb.toString();
-	}
-
-	@Override
-	public boolean isStochastic() {
-		return false;
-	}
-
-	@Override
-	public ArrayList<Move> unpack() {
-		return null;
 	}
 }
