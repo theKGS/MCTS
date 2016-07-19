f9ae49f1969f67add56a73cab751c790280f79b5
Ganryu <Ganryu@192.168.0.2>
2016-07-19 22:34:33 +0200

    Forgot to properly index two files.

---
+++ src/main/CallLocation.java
@@ -0,0 +1,6 @@
+package main;
+
+public enum CallLocation {
+	treePolicy,
+	playout
+}
---
+++ src/main/FinalSelectionPolicy.java
@@ -0,0 +1,6 @@
+package main;
+
+public enum FinalSelectionPolicy {
+	maxChild, // The child with highest SCORE
+	robustChild // The child with highest visit count
+}
