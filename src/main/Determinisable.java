72b6bf8e402fd65c8d564b94d983e1d981e21b21
ganryu <ganryu@Cirno>
2016-12-25 20:36:53 +0100

    Working on determinisation support.

--- src/main/Determinisable.java
+++ src/main/Determinisable.java
@@ -2,15 +2,6 @@ package main;
 
 public interface Determinisable extends Board {
 
-	/**
-	 * Make a copy of the board, but any hidden
-	 * information is randomized upon calling this
-	 * such that the new copy is an exact copy of
-	 * the original sans any information hidden
-	 * to the AI.
-	 * 
-	 * @return
-	 */
 	public Determinisable determinize();
 	
 }
