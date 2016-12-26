630dba3539fef871b074b65f0b79b98491f11b8c
ganryu <ganryu@Cirno>
2016-12-26 16:39:28 +0100

    Started work on card game

---
+++ src/cardgame/CardGame.java
@@ -0,0 +1,65 @@
+package cardgame;
+
+import java.util.ArrayList;
+
+import main.Board;
+import main.CallLocation;
+import main.Move;
+
+public class CardGame implements Board {
+
+	@Override
+	public Board duplicate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Move> getMoves(CallLocation location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void makeMove(Move m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean gameOver() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getCurrentPlayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getQuantityOfPlayers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double[] getScore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] getMoveWeights() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void bPrint() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
